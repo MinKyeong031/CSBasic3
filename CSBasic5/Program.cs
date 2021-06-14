f880a40aef98c7929077911a9860d3e9c585b0b8
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-06-14 16:10:54 +0900

    #11 생성자 실습 C#에는 정적 생성자가 있다!

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -8,6 +8,8 @@ namespace CSBasic5
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
             MultiClass ml = new MultiClass();
@@ -24,6 +26,53 @@ namespace CSBasic5
             ml.somePublic();
             //ml.someDefault();
             //ml.somePrivate();
+
+            Product productA = new Product("감자", 2000);
+            Product productB = new Product("고구마", 3000);
+            Console.WriteLine(productA);
+            Console.WriteLine(productB);
+            Console.WriteLine(Product.counter + "개 생성되었습니다.");
+
+            Console.WriteLine("첫 번째 위치");
+            Sample sample = new Sample();
+            //Console.WriteLine(Sample.value);
+            Console.WriteLine("두 번째 위치");
+            Console.WriteLine(Sample.value);
+            //Sample sample = new Sample();
+            Console.WriteLine("세 번째 위치");
+
+        }
+
+        class Sample
+        {
+            public static int value;
+            static Sample()
+            {
+                value = 10;
+                Console.WriteLine("정적 생성자 호출");
+            }
+        }
+
+        class Product
+        {
+
+            public static int counter = 0;
+            public int id;
+            public string name;
+            public int price;
+
+            public Product(string name, int price)
+            {
+                Product.counter = Product.counter + 1;
+                this.id = Product.counter;
+                this.name = name;
+                this.price = price;
+            }
+
+            public override string ToString()
+            {
+                return id + " : " + name;
+            }
         }
 
         class MyMath
@@ -64,6 +113,7 @@ namespace CSBasic5
             {
                 Console.WriteLine("private!");
             }
+
             public int Multi(int x, int y)
             {
                 return x * y;
@@ -78,22 +128,6 @@ namespace CSBasic5
             {
                 int output = 0;
                 for (int i = min; i <= max; i++)
-                {
-                    output += i;
-                }
-                return output;
-            }
-        }
-    }
-}
-public void Print()
-            {
-                Console.WriteLine("Print() 메서드 호출!");
-            }
-            public int Sum(int min, int max)
-            {
-                int output = 0;
-                for(int i = min; i <= max; i++)
                 {
                     output += i;
                 }
