bcf1493e21f9f8703f573006bdcfd89f5216915a
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-09-01 09:41:42 +0900

    인터페이스를 이용한 다중상속 예제

--- CSBasic8/Program.cs
+++ CSBasic8/Program.cs
@@ -8,8 +8,81 @@ namespace CSBasic8
 {
     class Program
     {
+        class Parent { }
+        class Child : Parent, IDisposable, IComparable
+        {
+            public int CompareTo(object obj)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class TestClass : IBasic
+        {
+            public int TestProperty
+            {
+                get => 0;
+                set => throw new NotImplementedException();
+            }
+
+            public int TestInstanceMethod()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class Dummy : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose() 메서드 호출!");
+            }
+        }
+        class Product : IComparable
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            public int CompareTo(object obj)
+            {
+                return -1 * Price.CompareTo((obj as Product).Price);
+                //return Name.CompareTo((obj as Product).Name);
+            }
+
+            public override string ToString()
+            {
+                return Name + ":" + Price + "원";
+            }
+        }
+
         static void Main(string[] args)
         {
+            Child child = new Child();
+            Parent childAsParent = new Child();
+            IDisposable childAsDisposable = new Child();
+            IComparable childAsComparable = new Child();
+
+            using (Dummy dummy = new Dummy())
+            {
+                Console.WriteLine("뭔가 작업을 했음. 주절주절");
+            }
+
+            List<Product> list = new List<Product>()
+            {
+                new Product(){Name="고구마", Price=1500 },
+                new Product(){Name="사과", Price=2400 },
+                new Product(){Name="바나나", Price=1000 },
+                new Product(){Name="배", Price=3000 },
+            };
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
